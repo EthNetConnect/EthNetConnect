84d6908d2e8cdf5cfa672ad045da5a2ec5ab60f2
J <j@curlysemi.com>
2018-07-28 15:46:27 -0700

    Sketch contract

--- contracts/ArizonaHistoryCourse.sol
+++ contracts/ArizonaHistoryCourse.sol
@@ -1,13 +1,53 @@
 pragma solidity ^0.4.18;
 
 contract ArizonaHistoryCourse {
-    uint storedData;
+    string private courseName;
+    string private courseFinalExamQuestion;
+    string private lesson;
+    bytes32 private examAnswerCommitment;
+    uint private courseCredits;
 
-    function set(uint x) public {
-        storedData = x;
+    mapping(address => uint) userProgress;
+
+    constructor(string name, string content, string finalExamQuestion, bytes32 answerCommitment, uint credits) {
+        courseName = name;
+        lesson = content;
+        courseFinalExamQuestion = finalExamQuestion;
+        examAnswerCommitment = answerCommitment;
+        courseCredits = credits;
     }
 
-    function get() public view returns (uint) {
-        return storedData;
+    function enroll() public payable {
+        if (userProgress[msg.sender] != 4) {
+            require(msg.value == courseCredits);
+        }
+        userProgress[msg.sender] = 1;
+    }
+
+    modifier onlyLevel(uint level) {
+        require(userProgress[msg.sender] == level);
+        _;
+    }
+
+    function getLesson() public view onlyLevel(1) returns (string) {
+        return lesson;
+    }
+
+    function getExamQuestion() public view onlyLevel(1) returns (string) {
+        return courseFinalExamQuestion;
+    }
+
+    function submit(string answerPreimage) public {
+        if (keccak256(abi.encodePacked(answerPreimage)) == examAnswerCommitment) {
+            userProgress[msg.sender] = 2;
+        }
+        else {
+            userProgress[msg.sender] = 4;
+        }
+    }
+
+    function getCertificate() public onlyLevel(2) {
+        userProgress[msg.sender] = 0;
+        msg.sender.transfer(courseCredits);
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var ArizonaHistoryCourse = artifacts.require("./ArizonaHistoryCourse.sol");
 
 module.exports = function(deployer) {
-    deployer.deploy(ArizonaHistoryCourse);
+    deployer.deploy(ArizonaHistoryCourse, "Arizona History Course", "Carl Hayden, a dead dude, became Arizona’s first congressman upon statehood.", "Who became Arizona’s first congressman upon statehood? 1) Carl Hayden 2) Daffy Duck", "0xc89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6", 4);
 };
